2e46fe837a8b370d759e62c3d0f4d9efc0543d9b
pelhombre <elhombepol120@gmail.com>
2023-10-18 19:48:25 +0200

    Update challenge2

---
+++ js/exercise-02.js
@@ -0,0 +1,10 @@
+// 1. Declare a variable using let with any name you like, but without a value.
+let message;
+
+// 2. On a separate line, assign the variable the String: "Moo!"
+message = "Moo!";
+
+// 3. Log out this variable to the terminal using console.log
+console.log(message);
+
+/*Repeat the above steps but try using a const variable. Comment this new part of code and explain what is happening.*/
---
+++ js/exercise-04.js
@@ -0,0 +1,29 @@
+/*What would each of these lines of code do:
+
+    1. true && console.log("moo");
+
+    Aquesta línia registrarà "mu" a la consola.
+    L'operador && realitza la lletra Y.
+    Ja que el costat esquerre és veritat, procedeix a executar el codi del costat dret.
+
+    2. false && console.log("moo moo?")
+
+    Aquesta línia no registra res a la consola.
+    L'operador && realitza una operació lògica I, i com que el costat esquerre és fals,
+    s'atura i no avalua ni executa el codi del costat dret.
+
+    3. true || console.log("hello friend");
+
+    Aquesta línia no registra res a la consola.
+    L'operador || realitza una operació lògica O.
+    Ja que el costat esquerre és cert, no cal avaluar el costat dret,
+    així que no executa l'expressió console.log.
+
+    4. false || console.log("bye friend");
+
+    Aquesta línia registrarà "adeu amic" a la consola. L'operador || realitza una operació lògica O. 
+    Com que el costat esquerre és fals, avalua el costat dret per determinar el resultat, 
+    i en aquest cas, executa l'expressió console.log.
+
+    Once you see the results, write in a comment why it works that way
+*/
