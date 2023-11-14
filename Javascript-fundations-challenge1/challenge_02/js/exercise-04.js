7416af63b3ebaae76176f9c31ea83bf100948cb5
pelhombre <elhombepol120@gmail.com>
2023-11-14 19:37:32 +0100

    Update escritoi github

---
+++ Javascript-fundations-challenge1/challenge_02/js/exercise-04.js
@@ -0,0 +1,31 @@
+/*What would each of these lines of code do:
+
+    1. true && console.log("moo"):
+
+    This line will log "moo" to the console.
+    The && operator performs a logical AND operation.
+    Since the left side is true, it proceeds to execute the code on the right side.
+
+    2. false && console.log("moo moo?"):
+
+    This line does not log anything to the console.
+    The && operator performs a logical AND operation, and because the
+    left side is false, it stops and does not evaluate or execute
+    the code on the right side.
+
+    3. true || console.log("hello friend"):
+
+    This line does not log anything to the console.
+    The || operator performs a logical OR operation.
+    Since the left side is true, there is no need to evaluate the 
+    right side, so it does not execute the console.log expression.
+
+    4. false || console.log("bye friend"):
+
+    This line will log "bye friend" to the console.
+    The || operator performs a logical OR operation.
+    Since the left side is false, it evaluates the right side to determine 
+    the result, and in this case, it executes the console.log expression.
+
+    Once you see the results, write in a comment why it works that way.
+*/
