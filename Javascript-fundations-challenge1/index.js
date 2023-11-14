504b6bc637a70bad1d356771546aff6cafbea2f1
pelhombre <elhombepol120@gmail.com>
2023-11-14 19:35:41 +0100

    Update escritori github

---
+++ Javascript-fundations-challenge1/index.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param {number} number 
+ * @returns boolean
+ */
+
+function isEven(number){
+
+    let isEven;
+
+    isEven = number % 2 === 0? true:false;
+
+    return isEven
+}
+
+console.log(isEven(2));
+console.log(isEven(3));
+    
